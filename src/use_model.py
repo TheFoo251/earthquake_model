6fe7ae1bb512f628b0dedc695e656cc2d20c5d40
Dawson Richards <dawson@thinkpad.my.domain>
2024-03-23 12:40:00 -0400

    rewrote unet to be used locally

---
+++ src/use_model.py
@@ -0,0 +1,47 @@
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+import keras
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, BatchNormalization, UpSampling2D
+from keras.layers import  Dropout, Activation
+from keras.optimizers import Adam, SGD
+from keras.layers import LeakyReLU
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras import backend as K
+from keras.utils import plot_model
+import tensorflow as tf
+import glob
+import random
+import cv2
+from random import shuffle
+
+MODEL_PATH = os.path.join("model", "earthquake_unet.keras")
+
+"""
+TESTING (using hold data)
+@TODO -- actually use hold data instead of testing image...
+"""
+model = keras.saving.load_model(MODEL_PATH, custom_objects=None, compile=True, safe_mode=True)
+
+raw = Image.open('test.jpg')
+raw = np.array(raw.resize((256, 256)))/255.
+raw = raw[:,:,0:3]
+
+#predict the mask
+pred = model.predict(np.expand_dims(raw, 0))
+
+#mask post-processing
+msk  = pred.squeeze()
+msk = np.stack((msk,)*3, axis=-1)
+msk[msk >= 0.5] = 1
+msk[msk < 0.5] = 0
+
+#show the mask and the segmented image
+combined = np.concatenate([raw, msk, raw* msk], axis = 1)
+plt.axis('off')
+plt.imshow(combined)
+plt.show()
+
