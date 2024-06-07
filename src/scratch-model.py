4ed57fa2c84546887099deea19bad285a81a3e1f
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-06-07 15:46:43 -0400

    start own model

--- src/scratch-model.py
+++ src/scratch-model.py
@@ -1,14 +1,15 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-data_dir = os.path.join(full_data)
+data_dir = os.path.join(data)
 
 train_ds, val_ds = keras.utils.image_dataset_from_directory(
     directory,
     labels=None,
     label_mode=None,
     class_names=None,
-    color_mode="rgb",
+    color_mode="rgba",
     batch_size=32,
     image_size=(1024, 1024),
     shuffle=True,
