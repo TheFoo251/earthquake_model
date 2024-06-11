981729a8b1c8f2e13d4d499f292cf15a8632c374
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-06-11 14:03:25 -0400

    yet more work on the torch model

--- src/train-torch-model.py
+++ src/train-torch-model.py
@@ -5,15 +5,15 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from torch-unet import UNET
-"""
-from utils import (
+
+from torch-utils import (
     load_checkpoint,
     save_checkpoint,
     get_loaders,
     check_accuracy,
     save_predictions_as_imgs
 )
-"""
+
 
 # Hyperparameters
 LEARNING_RATE = 1e-4
@@ -53,7 +53,7 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
     
 def main():
     #TODO -- look ub albumentations or whatever
-    train_transforms = A.Compose(
+    transforms = A.Compose(
         [
             A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH), # not necessary
             A.Rotate(limit=35, p=1.0),
@@ -67,7 +67,7 @@ def main():
             ToTensorV2(),
         ],
     )
-
+    """
     val_transforms = A.Compose(
         [   A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH), # not necessary
             A.Normalize( # basically just divides by 255
@@ -78,6 +78,7 @@ def main():
             ToTensorV2(),
         ],
     )
+    """
     
     # to generalize to more classes, just up out channels
     # and change to regular CE loss.
@@ -89,10 +90,9 @@ def main():
         IMG_DIR,
         MASK_DIR,
         BATCH_SIZE,
-        train_transforms,
-        val_transforms,
         NUM_WORKERS,
-        PIN_MEMORY
+        PIN_MEMORY,
+        transforms
     )
     
     scaler = torch.cuda.amp.GradScaler()
@@ -100,9 +100,19 @@ def main():
         train_fn(train_loader, model, optimizer, loss_fn, scaler)
         
         # save model
-        # check accuracy
-        # print some examples to a folder
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer":optimizer.state_dict(),
+        }
+        save_checkpoint(val_loader)
         
+        # check accuracy
+        check_accuracy(val_loader, model, device=DEVICE)
+
+        # print some examples to a folder
+        save_predictions_as_imgs(
+            val_loader, model, folder="saved_images/", device=DEVICE
+        )
     
 if __name__ == "__main__:
     main()
