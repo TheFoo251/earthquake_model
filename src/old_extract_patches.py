9d206aaa40ab10c681232020a7378879f1fbcd35
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-06-25 10:10:01 -0400

    added new patch extraction

--- src/old_extract_patches.py
+++ src/old_extract_patches.py
@@ -16,10 +16,7 @@ import glob
 if len(sys.argv) < 2 or sys.argv[1] == "help" or sys.argv[1] == "-h":
     print("Usage: python3 extract_patches.py patch_size input_dir")
     exit()
-    
 PATCH_SIZE = int(sys.argv[1])
-INPUT_SIZE = 1024
-
 INPUT_DIR = sys.argv[2]
 OUTPUT_DIR = f"patch_data_{PATCH_SIZE}"
 
@@ -34,25 +31,19 @@ MASKS = sorted(glob.glob(f"{INPUT_DIR}/targets/*"))
 # print(MASKS)
 
 
-def split(array):
-    """
-    Split a matrix into sub-matrices.
-    # https://stackoverflow.com/questions/11105375/how-to-split-a-matrix-into-4-blocks-using-numpy
-    
-    """
-    
-    nrows, ncols = INPUT_SIZE // PATCH_SIZE, INPUT_SIZE // PATCH_SIZE
-    r, h = array.shape
-    return (array.reshape(h//nrows, nrows, -1, ncols)
-                 .swapaxes(1, 2)
-                 .reshape(-1, nrows, ncols))
-
+# The "center" of 32x32 is 15 (0 indexing)
+HALF = PATCH_SIZE // 2
 
 
 def extract_patches(image_file_path, mask_file_path):
     """
     Extract patches given file paths
     """
+
+    # Internal function to calculate patch
+    def calc_patch(pixel_coords):
+        x, y = tuple(pixel_coords)
+        return (x - HALF + 1, y - HALF + 1, x + HALF + 1, y + HALF + 1)
 
     # extract image file names for later
     rgb_name = os.path.basename(image_file_path)[:-4]
@@ -62,27 +53,39 @@ def extract_patches(image_file_path, mask_file_path):
     # open up the images for later
     rgb_image = Image.open(image_file_path)
     mask_image = Image.open(mask_file_path)
-    
-    rgb_array = np.asarray(rgb_image)
-    mask_array = np.asarray(mask_image)
-    
-    rgb_sliced = split(rgb_array)
-    mask_sliced = split(mask_array)
-    
-    for i, rgb_arr, mask_arr in enumerate(zip(rgb_sliced, mask_sliced)):
-        
+
+    # create mask array to use
+    mask_array = np.array(mask_image)
+
+    # get coordinates of damaged pixels
+    damaged_pixels = np.argwhere(mask_array > 0)
+
+    # @TODO -- only work with pixels inside range and skip pixels
+    # for now, just try to get pixels that are within the borders- each point is within a border of the image.
+    # each values should be between 16 and 1008  (1024-16)
+
+    # interate over skip
+    SKIP = 10
+    total_patches = len(damaged_pixels) // SKIP
+    # print(f"Generating {num_patches} patches:")
+    for i, coords in enumerate(damaged_pixels[::SKIP]):
+        patch_box = calc_patch(coords)
+        a, b, c, d = patch_box
+
         # rgb
-        rgb_im = Image.fromarray(rgb_arr)
+        rgb_patch = rgb_image.crop(patch_box)
         filename = f"{rgb_name}--patch{i:05d}.png"
-        rgb_im.save(os.path.join(OUTPUT_DIR, "images", filename))
+        rgb_patch.save(os.path.join(OUTPUT_DIR, "images", filename))
 
         # mask
-        mask_im = Image.fromarray(mask_arr)
+        mask_patch = mask_image.crop(patch_box)
         filename = f"{mask_name}--patch{i:05d}.png".replace("_target", "")
-        mask_im.save(os.path.join(OUTPUT_DIR, "targets", filename))
+        mask_patch.save(os.path.join(OUTPUT_DIR, "targets", filename))
 
 
 if __name__ == "__main__":
+    # @DEBUG
+    # print(PATCH_SIZE)
     print("This script no longer removes class data")
 
     total_images = len(IMAGES)
@@ -94,3 +97,8 @@ if __name__ == "__main__":
         count += 1
 
     print("finished!")
+
+    # @DEBUG --  display overlay
+    # tensor_mask = np.stack((mask_array,)*3, axis=-1)
+    # overlay_image = Image.fromarray(rgb_array*tensor_mask)
+    # overlay_image.show()
