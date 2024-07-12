8977228eb2071b4ee7e9e1bf93ace8e55246cc5f
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-07-12 14:34:22 -0400

    prepare even binary classifier trainer

--- src/train_even_data_convnext_classifier_model.py
+++ src/train_even_data_convnext_classifier_model.py
@@ -21,42 +21,15 @@ import logging
 import sys
 
 # other files
-from torch_utils import get_loaders, get_even_loaders
+from torch_utils import get_even_loaders
+from torch_utils import plot_loss_curves
 
 cudnn.benchmark = True
 
-NUM_EPOCHS = 15
+NUM_EPOCHS = 30
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 PATCH_SZ, BATCH_SZ = 256, 16
-
-NUM_TRIALS = 40
-
-
-# losses
-
-
-# copied from https://github.com/ashawkey/FocalLoss.pytorch/blob/master/Explaination.md
-class FocalLoss(nn.Module):
-    """
-    Multi-class Focal Loss
-    """
-
-    def __init__(self, gamma=2, weight=None):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.weight = weight
-
-    def forward(self, input, target):
-        """
-        input: [N, C], float32
-        target: [N, ], int64
-        """
-        logpt = F.log_softmax(input, dim=1)
-        pt = torch.exp(logpt)
-        logpt = (1 - pt) ** self.gamma * logpt
-        loss = F.nll_loss(logpt, target, self.weight)
-        return loss
 
 
 def train_one_epoch(loader, model, optimizer, loss_fn, scaler, scheduler):
@@ -87,6 +60,8 @@ def train_one_epoch(loader, model, optimizer, loss_fn, scaler, scheduler):
         # update tqdm loop
         loop.set_postfix(loss=running_loss / (batch_idx + 1))
 
+    return running_loss / len(loader)
+
 
 def val_one_epoch(loader, model, loss_fn):
     loop = tqdm(loader)  # progress bar wrapped over dataloader
@@ -103,6 +78,8 @@ def val_one_epoch(loader, model, loss_fn):
 
             # update tqdm loop
             loop.set_postfix(val_loss=running_loss / (batch_idx + 1))
+
+    return running_loss / len(loader)
 
 
 # --- METRICS ---
@@ -122,85 +99,79 @@ def check_accuracy(loader, model):
     return accuracy
 
 
-# --- objective ---
-
 model_weights = models.ConvNeXt_Base_Weights.DEFAULT
 auto_transforms = model_weights.transforms()  # need these for pre-training
 dataloaders = get_even_loaders(PATCH_SZ, BATCH_SZ, transforms=auto_transforms)
 
 
-def objective(trial):
+lr = 9e-6  # from optimizer study, close to 1e-5
 
-    lr = trial.suggest_float("learning_rate", 1e-7, 1e-5)
+# here's where the transfer magic happens...
 
-    # here's where the transfer magic happens...
+model = models.convnext_base(weights=model_weights)
 
-    model = models.convnext_base(weights=model_weights)
+# this section copied from https://medium.com/exemplifyml-ai/image-classification-with-resnet-convnext-using-pytorch-f051d0d7e098
+n_inputs = None
+for name, child in model.named_children():
+    if name == "classifier":
+        for sub_name, sub_child in child.named_children():
+            if sub_name == "2":
+                n_inputs = sub_child.in_features
+n_outputs = 2
 
-    # this section copied from https://medium.com/exemplifyml-ai/image-classification-with-resnet-convnext-using-pytorch-f051d0d7e098
-    n_inputs = None
-    for name, child in model.named_children():
-        if name == "classifier":
-            for sub_name, sub_child in child.named_children():
-                if sub_name == "2":
-                    n_inputs = sub_child.in_features
-    n_outputs = 2
+sequential_layers = nn.Sequential(
+    LayerNorm2d((1024,), eps=1e-06, elementwise_affine=True),
+    nn.Flatten(start_dim=1, end_dim=-1),
+    nn.Linear(n_inputs, 2048, bias=True),
+    nn.BatchNorm1d(2048),
+    nn.ReLU(),
+    nn.Dropout(0.1),
+    nn.Linear(2048, 2048),
+    nn.BatchNorm1d(2048),
+    nn.ReLU(),
+    nn.Linear(2048, n_outputs),
+)
+model.classifier = sequential_layers
 
-    sequential_layers = nn.Sequential(
-        LayerNorm2d((1024,), eps=1e-06, elementwise_affine=True),
-        nn.Flatten(start_dim=1, end_dim=-1),
-        nn.Linear(n_inputs, 2048, bias=True),
-        nn.BatchNorm1d(2048),
-        nn.ReLU(),
-        nn.Dropout(0.1),
-        nn.Linear(2048, 2048),
-        nn.BatchNorm1d(2048),
-        nn.ReLU(),
-        nn.Linear(2048, n_outputs),
+model = model.to(DEVICE)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.AdamW(model.parameters(), lr=lr)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+scheduler = lr_scheduler.CosineAnnealingLR(
+    optimizer, T_max=math.floor(len(dataloaders["train"]) / BATCH_SZ)
+)
+
+scaler = torch.cuda.amp.GradScaler()
+
+metrics = {
+    "train_loss": [],
+    "val_loss": [],
+    "train_acc": [],
+    "val_acc": [],
+}
+
+
+# train it!
+for epoch in range(NUM_EPOCHS):
+    print(f"Epoch {epoch+1}/{NUM_EPOCHS}")
+    train_loss = train_one_epoch(
+        dataloaders["train"],
+        model,
+        optimizer,
+        loss_fn,
+        scaler,
+        scheduler=scheduler,
     )
-    model.classifier = sequential_layers
+    val_loss = val_one_epoch(model=model, loss_fn=loss_fn, loader=dataloaders["val"])
+    train_accuracy = check_accuracy(loader=dataloaders["train"], model=model)
+    val_accuracy = check_accuracy(loader=dataloaders["val"], model=model)
+    metrics["train_loss"].append(train_loss)
+    metrics["val_loss"].append(val_loss)
+    metrics["train_acc"].append(train_accuracy)
+    metrics["val_acc"].append(val_accuracy)
 
-    model = model.to(DEVICE)
-
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=lr)
-
-    # Decay LR by a factor of 0.1 every 7 epochs
-    scheduler = lr_scheduler.CosineAnnealingLR(
-        optimizer, T_max=math.floor(len(dataloaders["train"]) / BATCH_SZ)
-    )
-
-    scaler = torch.cuda.amp.GradScaler()
-
-    # train it!
-    for epoch in range(NUM_EPOCHS):
-        print(f"Epoch {epoch+1}/{NUM_EPOCHS}")
-        train_one_epoch(
-            dataloaders["train"],
-            model,
-            optimizer,
-            loss_fn,
-            scaler,
-            scheduler=scheduler,
-        )
-        val_one_epoch(model=model, loss_fn=loss_fn, loader=dataloaders["val"])
-        epoch_accuracy = check_accuracy(loader=dataloaders["val"], model=model)
-        print("accuracy:", epoch_accuracy)
-        trial.report(epoch_accuracy, epoch)
-
-    accuracy = check_accuracy(loader=dataloaders["val"], model=model)
-    return accuracy
-
-
-if __name__ == "__main__":
-
-    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-    study_name = "even-classifier-study-2"  # Unique identifier of the study.
-    storage_name = "sqlite:///{}.db".format(study_name)
-    study = optuna.create_study(
-        study_name=study_name,
-        storage=storage_name,
-        direction="maximize",
-        load_if_exists=True,
-    )
-    study.optimize(objective, n_trials=NUM_TRIALS)
+final_accuracy = check_accuracy(loader=dataloaders["val"], model=model)
+print("final accuracy:", final_accuracy)
+plot_loss_curves(metrics)
