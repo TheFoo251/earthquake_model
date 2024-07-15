350c75cf19e7e497f9308184b11655e7ac83368b
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-07-15 16:36:48 -0400

    having separate Learner class actually doesn't help too much...

--- src/train_even_data_convnext_classifier_model.py
+++ src/train_even_data_convnext_classifier_model.py
@@ -22,6 +22,103 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 PATCH_SZ, BATCH_SZ = 256, 16  # lower batch size?
 
 
+
+
+
+class Learner:
+    def __init__(
+        self, loader, model, optimizer, loss_fn, scaler, scheduler, device=DEVICE
+    ):
+        self.loader = loader
+        self.model = model.to(DEVICE)
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.scaler = scaler
+        self.scheduler = scheduler
+        self.device = device
+        self.metrics = {
+            "train_loss": [],
+            "val_loss": [],
+            "train_acc": [],
+            "val_acc": [],
+        }
+
+    def check_accuracy(self, loader):
+        correct = 0
+        self.model.eval()
+        with torch.no_grad():
+            for _, _, data, _, targets in loader:
+                data, targets = data.to(DEVICE), targets.to(DEVICE)
+                preds = self.model(data)
+                correct += torch.sum(targets == torch.argmax(preds, dim=1)).item()
+            accuracy = correct / len(loader.dataset)
+
+        self.model.train()
+        return accuracy
+
+    def train_one_epoch(self):
+        loop = tqdm(self.loader["train"])  # progress bar wrapped over dataloader
+        self.model.train(True)
+
+        running_loss = 0.0
+
+        for batch_idx, (_, _, data, _, targets) in enumerate(loop):
+            data, targets = data.to(self.device), targets.to(self.device)
+
+            self.optimizer.zero_grad()  # supposed to be first in this loop
+
+            # forward
+            with torch.cuda.amp.autocast():  # automatically casts as fp16
+                predictions = self.model(data)
+                loss = self.loss_fn(predictions, targets)
+
+            # metrics -- these go outside of autograd because they are not computationally intensive
+            running_loss += loss.item()
+
+            # backward (backward -> optimizer step -> scheduler step)
+            self.scaler.scale(loss).backward()  # backward pass using scaler
+            self.scaler.step(self.optimizer)  # optimizer step using scaler
+            self.scaler.update()  # updates for next iteration
+
+            # update tqdm loop
+            loop.set_postfix(loss=running_loss / (batch_idx + 1))
+
+        return running_loss / len(self.loader)
+
+    def val_one_epoch(self):
+        loop = tqdm(self.loader["val"])  # progress bar wrapped over dataloader
+        self.model.eval()
+
+        running_loss = 0.0
+
+        with torch.no_grad():
+            for batch_idx, (_, _, data, _, targets) in enumerate(loop):
+                data, targets = data.to(self.device), targets.to(self.device)
+                predictions = self.model(data)
+                loss = self.loss_fn(predictions, targets)
+                running_loss += loss.item()
+
+                # update tqdm loop
+                loop.set_postfix(val_loss=running_loss / (batch_idx + 1))
+
+        return running_loss / len(self.loader)
+
+    def train_model(self, num_epochs):
+        for epoch in range(num_epochs):
+            print(f"Epoch {epoch+1}/{num_epochs}")
+            train_loss = self.train_one_epoch()
+            val_loss = self.val_one_epoch()
+            self.scheduler.step(
+                val_loss
+            )  # this assumes reduceLRonplateu, unfortunately
+            train_accuracy = self.check_accuracy(loader=self.loader["train"])
+            val_accuracy = self.check_accuracy(loader=self.loader["val"])
+            self.metrics["train_loss"].append(train_loss)
+            self.metrics["val_loss"].append(val_loss)
+            self.metrics["train_acc"].append(train_accuracy)
+            self.metrics["val_acc"].append(val_accuracy)
+
+
 # --- METRICS ---
 
 
@@ -63,13 +160,7 @@ optimizer = optim.AdamW(model.parameters(), lr=lr)
 
 
 # Decay LR by a factor of 0.1 every 7 epochs
-scheduler = lr_scheduler.CosineAnnealingLR(
-    optimizer,
-    T_max=math.floor(
-        len(dataloaders["train"]) / BATCH_SZ
-    ),  # updates each batch, each epoch...
-)
-
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, "min")
 
 scaler = torch.cuda.amp.GradScaler()
 
@@ -80,6 +171,7 @@ learner = Learner(
     scaler=scaler,
     scheduler=scheduler,
     device=DEVICE,
+    loader=dataloaders,
 )
 
 learner.train_model(num_epochs=NUM_EPOCHS)
