45e1e1df2d8bc41e1531ca09edac8e87e4287e1d
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-07-15 11:14:44 -0400

    tweak transforms some

--- src/full_dataset.py
+++ src/full_dataset.py
@@ -66,15 +66,21 @@ class SiameseDataset(Dataset):
 convnext_transforms = {
     "image": v2.Compose(
         [
+            v2.ToImage(),
             v2.Resize(224),
-            v2.PILToTensor(),
-            v2.ToDtype(torch.float32, scale=True),
+            v2.ToDtype(torch.float32, scale=True),  # scale images to [0.0, 1.0]
+            v2.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),  # make sure to normalize to match ConvNext
         ]
     ),
     "mask": v2.Compose(
         [
+            v2.ToImage(),
             v2.Resize(224),
-            v2.PILToTensor(),
+            v2.ToDtype(
+                torch.float32, scale=False
+            ),  # make sure to turn off scaling for mask
         ]
     ),
 }
