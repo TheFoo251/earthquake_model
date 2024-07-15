fcf7f1d7f158235d1ce683bd257a6010a763f386
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-07-15 11:50:33 -0400

    change convnext transform handling to what it was

--- src/transforms.py
+++ src/transforms.py
@@ -1,29 +1,23 @@
 from torchvision.transforms import v2
 import torch
+import torchvision
+from torchvision import models
 
-# currently, this can't have any randomness, or the image/mask don't match anymore
+# matches https://pytorch.org/vision/main/models/generated/torchvision.models.convnext_base.html#torchvision.models.ConvNeXt_Base_Weights
 CONVNEXT = {
-    "image": v2.Compose(
-        [
-            v2.ToImage(),
-            v2.Resize(224),
-            v2.ToDtype(torch.float32, scale=True),  # scale images to [0.0, 1.0]
-            v2.Normalize(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            ),  # make sure to normalize to match ConvNext
-        ]
-    ),
+    "image": models.ConvNeXt_Base_Weights.IMAGENET1K_V1.transforms,
     "mask": v2.Compose(
         [
             v2.ToImage(),
-            v2.Resize(224),
             v2.ToDtype(
                 torch.float32, scale=False
             ),  # make sure to turn off scaling for mask
+            # resizing here is unecessary and throws off the creation of labels by removing small features
         ]
     ),
 }
 
+# currently, this can't have any randomness, or the image/mask don't match anymore
 DETECTOR = {
     "image": v2.Compose(
         [
@@ -45,3 +39,6 @@ DETECTOR = {
         ]
     ),
 }
+
+if __name__ == "__main__":
+    print(CONVNEXT["image"])
